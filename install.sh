e168ff28bd23e1b33f16503e1dada992f4acc204
Arjun Rao <arjun210493@gmail.com>
2017-07-05 14:54:46 +0200

    Using NEST default directories - undocumented

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-cmake .
-cmake --build .
+#!/usr/bin/env bash
+
+if [ ! -d ./build_LookbackModule ]; then
+	mkdir ./build_LookbackModule
+fi
+
+cd ./build_LookbackModule
+cmake -Dwith-nest=`which nest-config` -DCMAKE_CXX_FLAGS='-g -Wno-unused-variable -Wno-reorder' ..
+make -j4
+make install
+cd ..
